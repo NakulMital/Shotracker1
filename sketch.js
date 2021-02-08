20d19976a89ce4571c688197e5cca963c89d3144
NakulMital <66588350+NakulMital@users.noreply.github.com>
2021-02-08 21:48:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,8 +2,10 @@ gameState = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  if (windowWidth === 414 && windowHeight === 846){
   screen1 = new Screen();
   screen1.display();
+}
 }
 
 function draw() {
