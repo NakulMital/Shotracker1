e477162c027045cebc340df83200421c28729ebc
NakulMital <66588350+NakulMital@users.noreply.github.com>
2021-02-22 11:40:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
 gameState = 1;
 var database;
 var cross;
-var circle;
 var x = 0;
 var y = 0;
 var makeImg;
 var missImg;
 
-function preload() {
-    circle = loadImage("Images/circle.png")
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
