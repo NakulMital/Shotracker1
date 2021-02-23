599025a8516b6018a20b3a6a95005c1561db8761
NakulMital <66588350+NakulMital@users.noreply.github.com>
2021-02-23 10:46:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+gameState = 1;
+var database;
+var cross;
+var x = 0;
+var y = 0;
+var makeImg;
+var missImg;
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+    
+  if (windowWidth>= 1200) {
+      screen1 = new Screen();
+      screen1.display();
+    }
+  else if (windowWidth>=768 && windowWidth<992 && windowWidth<windowHeight){
+    screen2 = new Screen2();
+    screen2.display();
+  }
+    
+  
+  
+  
+}
+
+function draw() {
+  console.log(x+" and "+y);
+}
